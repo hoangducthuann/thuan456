a7aeecc107525d6bdc158debc1e013615127509e
hoangthanh <lehoangnd@gmail.com>
2021-08-10 16:24:40 +0700

    thuantaora

---
+++ main.js
@@ -0,0 +1,27 @@
+/** Toán tử gián 
+  Toán tử           Ví dụ                    Tương đương                 
+  =                 x = y                     x = y
+  +=                X += y                    x = x + y
+  -=                x -= y                    x = x - y
+  *=                x *= y                    x = x * y
+  /=                x /= y                    x = x / y
+  **=               x **= y                   x = x ** Y
+
+ */
+
+//  var a = 1;
+
+// a -= 2;
+
+// console.log(a);
+
+/**
+ * Toán tử chuỗi - string operator
+ */
+ var firstName = 'Thuan'
+ var lastName = 'đệp TRoai'
+ console.log(firstName + ' ' + lastName);
+
+
+
+
